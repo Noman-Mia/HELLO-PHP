860aae9510045ca7db57c925fbdeb031a8764524
Noman Mia <nomanislamnishad@gmail.com>
2024-10-04 09:37:22 +0600

    added if-else

---
+++ Module-4/1.todo.php
@@ -0,0 +1,10 @@
+<?php 
+//if else
+$age = 60;
+
+if($age <= 60){
+    echo"Iam young";
+}
+else{
+    echo"Iam older";
+};
---
+++ Module-4/variable.php
@@ -0,0 +1,14 @@
+<?php
+// VARIABLE
+$ourCountry = "Bangladesh";
+$question = "How are you";
+$age = "20";
+
+echo $ourCountry;
+echo"\n";
+echo $question;
+echo"\n";
+
+// echo"Hello $ourCountry";
+// echo"Hello $ourCountry $question";
+echo"Hello {$ourCountry}, {$question}";
